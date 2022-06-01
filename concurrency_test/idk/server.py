ffecbc32d4117957bdb246b09f9fc294ac230fce
LucasTom-Wong <lgtw3450@gmail.com>
2022-06-01 08:36:27 -0400

    concurrency sort of, need to fix the leaving

--- concurrency_test/idk/server.py
+++ concurrency_test/idk/server.py
@@ -1,28 +1,24 @@
 import random
 import websockets
 import asyncio
+import json
+
+connected_people = set()
 
 async def listen(ws, path):
     async for message in ws:
-        await ws.send("Hi back!")
-
-# async def connected_message(ws, path):
-#     # print("Client just connected!")
-#     # connected.add(ws)
-#     try:
-#         async for message in ws:
-#             await ws.send("New Connection")
-#             # print("Recieved message from client: " + message)
-#             # for cos in connected:
-#             #     if cos != ws:
-#             #         await cos.send("Someone said: " + message)
-#     except websockets.exceptions.ConnectionClosed as e:
-#         await ws.send("Disconnected")
-#         # print(e)
+        for users in connected_people.copy():
+            if (users == ws): #sends if user that sent message
+                await users.send("Hi back!")
+            else:
+                await users.send("Hi there!")
+        # await ws.send("Hi back!")
 
 async def random_number(ws, path):
     while True:
-        await ws.send(str(int(random.random() * 20)))
+        response = str(int(random.random()*20))
+        for users in connected_people.copy(): #sends every user
+            await users.send(response)
         await asyncio.sleep(3)
 
 async def messenger(ws, path):
@@ -31,7 +27,18 @@ async def messenger(ws, path):
         await asyncio.sleep(5)
 
 async def full_all(ws, path):
-    await asyncio.gather(messenger(ws, path), random_number(ws, path), listen(ws, path))
+    connected_people.add(ws)
+    for users in connected_people.copy():
+        if (users != ws):
+            await users.send("New User has joined")
+    try:
+        await asyncio.gather(messenger(ws, path), random_number(ws, path), listen(ws, path))
+        # for user in connected_people.copy():
+        #     await asyncio.gather(messenger(user, path), random_number(user, path), listen(user, path))
+    except websockets.exceptions.ConnectionClosed as e:
+        connected_people.remove(ws)
+        for users in connected_people.copy():
+            await users.send("User has left")
     # await messenger(ws, path)
     # await random_number(ws, path)
     # await connected_message(ws, path)
