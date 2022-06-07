de1e0bb1efc2f465de95f1035d6bc0690283eb81
tomas-20 <tacuna20@stuy.edu>
2022-06-07 09:22:11 -0400

    moved the card code from __init__.py to the new cards.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from os import urandom
 from flask import Flask, render_template, request, session, redirect
 from db import *
+from cards import createCardList
 import sqlite3, os.path
 import json
 import urllib
@@ -82,34 +83,6 @@ def register():
         return redirect("/login")
 
     return render_template("register.html")
-
-ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-suites = ["C", "D", "H", "S"]
-allCards = []
-
-for rank in ranks:
-  for suite in suites:
-    allCards.append("/static/card_svgs/"+rank + suite + ".svg")
-
-def createCardList(player_num):
-    cardList = []
-    player = 0
-    index = 0
-    while (player < player_num):
-        cardList.append("/static/card_svgs/back.svg")
-        cardList.append("/static/card_svgs/back.svg")
-        index+=2
-        player+=1
-    cardList.append(allCards[index])
-    cardList.append(allCards[index+1])
-    index+=2
-    player+=1
-    while (player < 5):
-        cardList.append("/static/card_svgs/back.svg")
-        cardList.append("/static/card_svgs/back.svg")
-        player+=1
-    print(cardList)
-    return cardList
 
 @app.route("/poker", methods=['GET', 'POST'])
 def game():
---
+++ app/cards.py
@@ -0,0 +1,27 @@
+ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+suites = ["C", "D", "H", "S"]
+allCards = []
+
+for rank in ranks:
+  for suite in suites:
+    allCards.append("/static/card_svgs/"+rank + suite + ".svg")
+
+def createCardList(player_num):
+    cardList = []
+    player = 0
+    index = 0
+    while (player < player_num):
+        cardList.append("/static/card_svgs/back.svg")
+        cardList.append("/static/card_svgs/back.svg")
+        index+=2
+        player+=1
+    cardList.append(allCards[index])
+    cardList.append(allCards[index+1])
+    index+=2
+    player+=1
+    while (player < 5):
+        cardList.append("/static/card_svgs/back.svg")
+        cardList.append("/static/card_svgs/back.svg")
+        player+=1
+    print(cardList)
+    return cardList
