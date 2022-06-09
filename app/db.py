42436c544a43dbe3f91a4d5de957348472f69003
tomas-20 <tacuna20@stuy.edu>
2022-06-09 01:24:39 -0400

    consolidated the users and profiles tables into a single users table

--- app/db.py
+++ app/db.py
@@ -6,22 +6,15 @@ DB_FILE="users.db"
 def createTables():
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
-
     command = """CREATE TABLE IF NOT EXISTS users(
         username TEXT NOT NULL,
         password TEXT NOT NULL,
-        ID INTEGER NOT NULL)
-    """
-    c.execute(command)
-
-    command2 = """CREATE TABLE IF NOT EXISTS profiles(
-        ID INTEGER NOT NULL,
         wins INTEGER NOT NULL,
         losses INTEGER NOT NULL,
         money INTEGER NOT NULL,
         timesBroke INTEGER NOT NULL)
     """
-    c.execute(command2)
+    c.execute(command)
     db.commit();
     db.close();
 
@@ -30,19 +23,7 @@ createTables();
 def addUser(username, password):
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
-    c.execute("INSERT INTO users VALUES(?, ?, ?)", (username, password, createID()))
-    db.commit()
-    db.close()
-
-def createID():
-    db = sqlite3.connect(DB_FILE)
-    c = db.cursor()
-    c.execute("SELECT ID FROM users")
-    idList = c.fetchall()
-    id = random.randint(10000, 99999)
-    while id in idList:
-        id = random.randint(10000, 99999)
-    return id
+    c.execute("INSERT INTO users VALUES(?, ?, 0, 0, 0, 0)", (username, password))
     db.commit()
     db.close()
 
@@ -76,19 +57,19 @@ def addProfile(ID):
     db.commit()
     db.close()
 
-def updateProfileMoney(ID, amount):
+def updateUserMoney(username, amount):
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
-    c.execute("UPDATE profiles SET money = money + (?) WHERE ID = (?)", (amt, ID))
+    c.execute("UPDATE users SET money = money + (?) WHERE username = (?)", (amt, username))
     db.commit()
     db.close()
 
-def updateProfileWin(ID, boo):
+def updateUserWinLoss(username, boo):
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
     if (boo):
-        c.execute("UPDATE profiles SET wins = wins + 1 WHERE ID = (?)", (ID))
+        c.execute("UPDATE users SET wins = wins + 1 WHERE username = (?)", (username,))
     else:
-        c.execute("UPDATE profiles SET losses = losses + 1 WHERE ID = (?)", (ID))
+        c.execute("UPDATE users SET losses = losses + 1 WHERE username = (?)", (username,))
     db.commit()
     db.close()
